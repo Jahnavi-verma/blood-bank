cdca92a91cf8eb208a606416978343be664bd4dd
jahnavi Verma <jahnaviverma.cs24@bmsce.ac.in>
2025-10-15 16:20:39 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 import Aurora from './Aurora';
-
+import Beams from './beams';
+import './globals.css';
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div>
-        <Aurora
-          colorStops={["#660001", "#013221", "#00563E"]}
-          blend={0.5}
-          amplitude={1.0}
-          speed={0.5}
+    <main >
+      <div className="container">
+      <div className="background-div" style={{ width: '100%', height: '600px'}}>
+        <Beams
+          beamWidth={2}
+          beamHeight={15}
+          beamNumber={12}
+          lightColor="#fa181cff"
+          speed={2}
+          noiseIntensity={1.75}
+          scale={0.2}
+          rotation={0}
         />
       </div>
-      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black">
-        <p className="antialiased text-xl text-gray-800 md:text-3xl md:leading-normal">
-          Welcome to the Blood Bank Management System. Please log in or sign up to continue.
-        </p>
+      <p className="overlay-div">
+        Welcome to the BloodLine. Please log in continue.
+      </p>
       </div>
     </main>
   );
 }
-
