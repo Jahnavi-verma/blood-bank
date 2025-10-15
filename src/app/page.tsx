9d2c99d06bbe60dbc5245c9614660c21b929584f
jahnavi Verma <jahnaviverma.cs24@bmsce.ac.in>
2025-10-15 19:50:44 +0530

    trying to add login button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+'use client';
 import Aurora from './Aurora';
 import Beams from './beams';
 import './globals.css';
+import Dock from './dock';
+import LoginButton from "./loginbutton";
+
 export default function HomePage() {
   return (
     <main >
+      <html>
+        <body>
       <div className="container">
       <div className="background-div" style={{ width: '100%', height: '600px'}}>
         <Beams
           beamWidth={2}
           beamHeight={30}
           beamNumber={12}
-          lightColor="#fa181cff"
+          lightColor="#c96f70ff"
           speed={2}
           noiseIntensity={1.75}
           scale={0.2}
@@ -21,6 +27,11 @@ export default function HomePage() {
         Welcome to the BloodLine. Please log in continue.
       </p>
       </div>
+      <div>
+        <LoginButton onClick={() => alert("Login clicked")} />
+      </div>
+      </body>
+      </html>
     </main>
   );
 }
